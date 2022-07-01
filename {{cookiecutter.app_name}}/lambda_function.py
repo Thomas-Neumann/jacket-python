278ff604a8f42b2194e56cb517fb79c2d2f39007
Thomas Neumann <Thomas_Neumann@hey.com>
2022-07-01 13:42:21 +0200

    add convenience function 'read_yamlfile()'

--- {{cookiecutter.app_name}}/lambda_function.py
+++ {{cookiecutter.app_name}}/lambda_function.py
@@ -15,6 +15,8 @@ import sys
 
 # extra imports (make sure to add them to requirements.txt!)
 # <add extra imports here, e.g. 'boto3' 'requests'>
+import boto3
+import yaml
 
 from typing import Dict, Optional
 
@@ -24,6 +26,16 @@ sys.path.insert(0, os.path.dirname(__file__) + "/src")
 import {{cookiecutter.pypackage_name}}
 
 LH = logging.getLogger()
+
+
+def read_yamlfile(s3_bucket: str, s3_object: str) -> Dict:
+    """
+    read file from S3 and parse as YAML
+    """
+    s3_res = boto3.resource('s3')
+    s3_obj = s3_res.Object(s3_bucket, s3_object)
+    data = s3_obj.get()['Body'].read()
+    return json.loads(data.decode('utf-8'))
 
 
 def lambda_handler(event: Dict, context: Dict) -> Optional[Dict]:
